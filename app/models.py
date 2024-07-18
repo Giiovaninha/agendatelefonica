ad9ad886365b4e6b922b45843ce2054905d66b4f
Giovana Vitória Teodoro <140285473+Giiovaninha@users.noreply.github.com>
2024-07-17 21:35:48 -0300

    Add files via upload

--- app/models.py
+++ app/models.py
@@ -1,27 +1,38 @@
 from django.db import models
 
-# Create your models here.
 class Grupo(models.Model):
     nome = models.CharField(max_length=50, unique=True)
     descricao = models.CharField(max_length=280, null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
 
 class Contato(models.Model):
     nome = models.CharField(max_length=50, unique=True)
     sobrenome = models.CharField(max_length=50, unique=True)
     foto = models.ImageField(upload_to='fotos/', null=True, blank=True)
     grupos = models.ManyToManyField(Grupo)
-    cpf = models.CharField(max_length=14)  # Ajustei o max_length para CPF padrão
+    cpf = models.CharField(max_length=14)  
 
     class Meta:
         ordering = ['nome']
 
+    def __str__(self):
+        return f"{self.nome} {self.sobrenome}"
+
 class Telefone(models.Model):
     numero = models.CharField(max_length=20)
-    contatos = models.ForeignKey(Contato, on_delete=models.CASCADE)
+    contato = models.ForeignKey(Contato, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.numero
 
 class Email(models.Model):
     endereco = models.EmailField(max_length=255)
     contato = models.ForeignKey(Contato, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.endereco
 
 class Usuario(models.Model):
     nome = models.CharField(max_length=200)
@@ -29,12 +40,21 @@ class Usuario(models.Model):
     senha = models.CharField(max_length=128)
     email = models.ForeignKey(Email, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.nome
+
 class Pesquisa(models.Model):
     contato = models.ForeignKey(Contato, on_delete=models.CASCADE)
     grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Pesquisa de {self.contato.nome} sobre {self.grupo.nome}"
 
 class Chamada(models.Model):
     contato = models.ForeignKey(Contato, on_delete=models.CASCADE)
     data = models.DateField()
     horario = models.TimeField()
-    duracao = models.DurationField()  
+    duracao = models.DurationField()
+
+    def __str__(self):
+        return f"Chamada de {self.contato.nome} em {self.data} às {self.horario}"
--- app/views.py
+++ app/views.py
@@ -1,3 +1,61 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Contato, Grupo, Telefone, Email, Usuario, Pesquisa, Chamada
 
-# Create your views here.
+def contatos_list_view(request):
+    contatos = Contato.object.all()
+    return render(request, 'contatos_list.html', {'contatos': contatos})
+
+def contato_detail(request, pk):
+    contato = get_object_or_404(Contato, pk=pk)
+    return render(request, 'contato_detail.html', {'contato': contato})
+
+def grupos_list(request):
+    grupos = Grupo.objects.all()
+    return render(request, 'grupos_list.html', {'grupos': grupos})
+
+def grupo_detail(request, pk):
+    grupo = get_object_or_404(Grupo, pk=pk)
+    return render(request, 'grupo_detail.html', {'grupo': grupo})
+
+def telefones_list(request):
+    telefones = Telefone.objects.all()
+    return render(request, 'telefones_list.html', {'telefones': telefones})
+
+def telefone_detail(request, pk):
+    telefone = get_object_or_404(Telefone, pk=pk)
+    return render(request, 'telefone_detail.html', {'telefone': telefone})
+
+def emails_list(request):
+    emails = Email.objects.all()
+    return render(request, 'emails_list.html', {'emails': emails})
+
+def email_detail(request, pk):
+    email = get_object_or_404(Email, pk=pk)
+    return render(request, 'email_detail.html', {'email': email})
+
+def usuarios_list(request):
+    usuarios = Usuario.objects.all()
+    return render(request, 'usuarios_list.html', {'usuarios': usuarios})
+
+def usuario_detail(request, pk):
+    usuario = get_object_or_404(Usuario, pk=pk)
+    return render(request, 'usuario_detail.html', {'usuario': usuario})
+
+def pesquisas_list(request):
+    pesquisas = Pesquisa.objects.all()
+    return render(request, 'pesquisas_list.html', {'pesquisas': pesquisas})
+
+def pesquisa_detail(request, pk):
+    pesquisa = get_object_or_404(Pesquisa, pk=pk)
+    return render(request, 'pesquisa_detail.html', {'pesquisa': pesquisa})
+
+def chamadas_list(request):
+    chamadas = Chamada.objects.all()
+    return render(request, 'chamadas_list.html', {'chamadas': chamadas})
+
+def chamada_detail(request, pk):
+    chamada = get_object_or_404(Chamada, pk=pk)
+    return render(request, '/chamada_detail.html', {'chamada': chamada})
+
+
