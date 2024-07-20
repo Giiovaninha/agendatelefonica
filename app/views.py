ec9160aef0d30701546645343cac540a7e68579c
Giovana Vitória Teodoro <140285473+Giiovaninha@users.noreply.github.com>
2024-07-20 17:06:04 -0300

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -1,46 +1,46 @@
 from django.shortcuts import render
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import *
 
 
 def base(request):
     return render(request,'template')
 
-def contato_list(request, pk):
-    contato = get_object_or_404(Contato, pk=pk)
-    return render(request, 'template.html', {'contato': contato})
+def contato_list_view(request):
+    contato = Contato.objects.all()
+    return render(request, 'app/contatos_list.html', {'contatos': contato})
 
 
-def grupo_list(request, pk):
-    grupo = get_object_or_404(Grupo, pk=pk)
-    return render(request, 'grupo_list.html', {'grupo': grupo})
+# def grupo_list(request, pk):
+#     grupo = get_object_or_404(Grupo, pk=pk)
+#     return render(request, 'grupo_list.html', {'grupo': grupo})
 
 
-def telefone_list(request, pk):
-    telefone = get_object_or_404(Telefone, pk=pk)
-    return render(request, 'telefone_list.html', {'telefone': telefone})
-
-
-
-def email_list(request, pk):
-    email = get_object_or_404(Email, pk=pk)
-    return render(request, 'email_list.html', {'email': email})
+# def telefone_list(request, pk):
+#     telefone = get_object_or_404(Telefone, pk=pk)
+#     return render(request, 'telefone_list.html', {'telefone': telefone})
 
 
 
-def usuario_list(request, pk):
-    usuario = get_object_or_404(Usuario, pk=pk)
-    return render(request, 'usuario_list.html', {'usuario': usuario})
+# def email_list(request, pk):
+#     email = get_object_or_404(Email, pk=pk)
+#     return render(request, 'email_list.html', {'email': email})
 
 
 
-def pesquisa_list(request, pk):
-    pesquisa = get_object_or_404(Pesquisa, pk=pk)
-    return render(request, 'pesquisa_list.html', {'pesquisa': pesquisa})
+# def usuario_list(request, pk):
+#     usuario = get_object_or_404(Usuario, pk=pk)
+#     return render(request, 'usuario_list.html', {'usuario': usuario})
 
 
-def chamada_list(request, pk):
-    chamada = get_object_or_404(Chamada, pk=pk)
-    return render(request, '/chamada_list.html', {'chamada': chamada})
+
+# def pesquisa_list(request, pk):
+#     pesquisa = get_object_or_404(Pesquisa, pk=pk)
+#     return render(request, 'pesquisa_list.html', {'pesquisa': pesquisa})
+
+
+# def chamada_list(request, pk):
+#     chamada = get_object_or_404(Chamada, pk=pk)
+#     return render(request, '/chamada_list.html', {'chamada': chamada})
 
 
